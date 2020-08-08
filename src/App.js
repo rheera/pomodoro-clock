3e4253dfba2374997e286cb41524a8e37993489c
Raman Heera <rheera2@uwo.ca>
2020-08-08 18:56:13 -0400

    Update App.js
    - setting up static page with components for each element
    - started the timer component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,59 @@
 import React from 'react';
 
+
+
+class Time extends React.Component {
+    render() {
+        return (
+            <div id={"timer-div"}>
+
+            </div>
+        )
+    }
+}
+
+class Wheel extends React.Component {
+    render() {
+        return (
+            <div id={"wheel-div"}>
+                <div className="base-timer">
+                    <svg className="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+                        <g className="base-timer__circle">
+                            <circle className="base-timer__path-elapsed" cx="50" cy="50" r="45"/>
+                        </g>
+                    </svg>
+                    <span>
+    <!-- Remaining time label -->
+                    </span>
+                </div>
+            </div>
+        )
+    }
+}
+
+class Clock extends React.Component {
+    render() {
+        return (
+            <div id={"clock-div"}>
+                <h2>Session/Break</h2>
+                <Wheel />
+                <Time />
+            </div>
+        )
+    }
+}
+
+
+class Settings extends React.Component {
+    render() {
+        return (
+            <div id={"settings-div"}>
+
+            </div>
+        )
+    }
+}
+
 class AppWrapper extends React.Component {
     constructor(props) {
         super(props);
@@ -14,7 +68,8 @@ class AppWrapper extends React.Component {
             <div className={"container-fluid"}>
                 <div className={"d-flex justify-content-md-center align-items-center vh-100"}>
                     <div>
-                        Hi
+                        <Clock />
+                        <Settings />
                     </div>
                 </div>
             </div>
